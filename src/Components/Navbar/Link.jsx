5cbcb9d528b0d6dfcc83fca98043e136aefac36d
mdabdulaziz6236 <md.abdulaziz6236@gmail.com>
2025-09-28 08:00:22 +0600

    Complited navbar with responsive with absolute position and created a new folder for PricingOptions and a file for it and also created a Jsonfile for PricingData.json for Pricing

--- src/Components/Navbar/Link.jsx
+++ src/Components/Navbar/Link.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
-const Link = ({route}) => {
+export const Link = ({route}) => {
     return (
-        <li className='mr-5'>
+        <li className='px-3'>
             <a href={route.path}>{route.name}</a>
         </li>
     );
 };
 
-export default Link;
+
+export const LinkMd = ({route}) =>{
+    return(
+        <li  className=' px-3 bg-green-500 text-gray-800 py-1 hover:bg-yellow-400 border-2 border-blue-400 rounded-2xl'>
+            <a href={route.path}>{route.name}</a>
+        </li>
+    )
+}
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,53 +1,68 @@
-import React from 'react';
-import Link from './Link';
+import React, { useState } from "react";
+import {Link, LinkMd} from "./Link";
+import { Menu, X } from "lucide-react";
 const urls = [
   {
     id: 1,
     name: "Home",
-    path: "/"
+    path: "/",
   },
   {
     id: 2,
     name: "About",
-    path: "/about"
+    path: "/about",
   },
   {
     id: 3,
     name: "Services",
-    path: "/services"
+    path: "/services",
   },
   {
     id: 4,
     name: "Blog",
-    path: "/blog"
+    path: "/blog",
   },
   {
     id: 5,
     name: "Contact",
-    path: "/contact"
-  }
+    path: "/contact",
+  },
 ];
 
 const Navbar = () => {
-    return (
-        <nav>
-            <ul className='flex'>
-                {
-                    urls.map(route => <Link key={route.id} route={route}></Link>)
-                }
-            </ul>
-            {/* <ul className='flex '>
+    const links = urls.map((route) => (
+          <Link key={route.id} route={route}></Link>
+        ))
+    const links2 = urls.map((route) => (
+          <LinkMd
+           key={route.id} route={route}></LinkMd>
+        ))
+    const [open , setOpen] = useState(true)
+  return (
+    <nav className="flex my-3 justify-between w-11/12 mx-auto text-black">
+      <span className="flex" onClick={() => setOpen(!open)}>
+        {open ? <Menu className="md:hidden"></Menu> : <X className="font-bold"></X>}
+        <ul className={`md:hidden absolute duration-1000 ${open ?'-top-50 ': 'top-23 '}`}>
+            {links2}
+        </ul>
+        <h3 className="ml-3">Navbar</h3>
+      </span>
+      <ul className="md:flex hidden ">
+        {links}
+      </ul>
+      {/* <ul className='flex '>
                 {
                     urls.map(route => <li className='mr-10'><a href={route.path}>{route.name}</a></li>)
                 }
             </ul> */}
-            {/* <ul className='flex'>
+      {/* <ul className='flex'>
                 <li className='mr-10'><a href="/about">About</a></li>
                 <li className='mr-10'><a href="/blog">Blog</a></li>
                 <li className='mr-10'><a href="/">Home</a></li>
             </ul> */}
-        </nav>
-    );
+      <button>Sing In</button>
+    </nav>
+  );
 };
 
 export default Navbar;
